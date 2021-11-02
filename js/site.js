68c36025372b528d67f3d345e62ac3734aaafd24
Angie <77899962+aciturbide@users.noreply.github.com>
2021-11-02 16:53:52 -0500

    js

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@ function validate_form ( )
 {
     valid = true;
 
-    if ( document.contact_form.contact_name.value == "" )
+    if (.input == "" )
     {
         alert ( "Please fill in the 'Your Name' box." );
         valid = false;
